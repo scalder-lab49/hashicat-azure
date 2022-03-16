62ca6727dbefcc3763f37abda8f87f30974fd93b
Sam Calder <sam.calder@lab49.com>
2022-03-16 03:31:52 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -1,10 +1,5 @@
 module "network" {
   source  = "app.terraform.io/example-org-506960/network/azurerm"
   version = "3.5.0"
-  resource_group_name = "scalder-lab49-workshop"
-}
-
-resource "azurerm_resource_group" "scalder-lab49-workshop" {
-  name     = "scalder-lab49-workshop"
-  location = "southeastasia"
+  resource_group_name = "${var.prefix}-workshop"
 }
