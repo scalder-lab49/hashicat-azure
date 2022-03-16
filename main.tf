31131ceec9cf88098f9c8b5d04b27e63b6c3dde7
Sam Calder <sam.calder@lab49.com>
2022-03-16 02:58:54 +0000

    Added the second tag

--- main.tf
+++ main.tf
@@ -136,7 +136,8 @@ resource "azurerm_virtual_machine" "catapp" {
   }
 
   tags = {
-    Department = "devos"
+    Department = "devops"
+    Billable = "true"
   }
 
   # Added to allow destroy to work correctly.
